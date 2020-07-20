fbcfa82b41d8bdc3d771f42d0d2c4cc37e81af1b
Dom <61214047+dominiquedawez@users.noreply.github.com>
2020-07-20 13:01:08 -0400

    created mysql schemas connecected schemas to mysql database along with the server

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheese Burger', 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Feta Cheese Onion and Collard Burger', 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Tofu Burger', 0);
